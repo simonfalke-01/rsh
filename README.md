ef75604a602b8959c9b6174bcb3ecaa7b7ba2e1d
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-08-04 20:45:25 +0800

    smol update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rsh
 rsh - Rust Shell (Maybe Secure)
+
+A rust front-end for ssh.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let pi: f32 = 3.141592;
+    let mut input = String::new();
     println!("{pi:.5}", pi = pi);
     println!("Dumbass this is not finished");
 }
