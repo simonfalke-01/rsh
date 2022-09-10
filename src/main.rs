1c737d813c3f8323e61cb6a17e6a81f576859f5e
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-09-10 13:20:45 +0800

    mhm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::io;
+
 fn main() {
     let pi: f32 = 3.141592;
     let mut input = String::new();
     println!("{pi:.5}", pi = pi);
+    println!("Pls type smth");
+    io::stdin().read_line(&mut input).expect("Broe we have a issue contact local indian tech support");
+    println!("{input}");
     println!("Dumbass this is not finished");
 }
