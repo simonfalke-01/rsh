d7d7eb3a3d4631f5b7f9b6b71a6fdca6a68e10de
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-10-13 13:40:33 +0800

    wierd shit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, array};
 
 fn main() {
     let pi: f32 = 3.141592;
@@ -9,7 +9,7 @@ fn main() {
     println!("{}", input.trim());
     if input.trim() == "neofetch"
     {
-        println!("Broe we haven't connected stop tryin to flex on other ppl");
+        println!("Broe we haven't connected stop tryin to flex on other ppl u script kiddie");
     }
     println!("Dumbass this is not finished");
 }
