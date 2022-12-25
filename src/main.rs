ba6873ce279158406655580cf0fb3a665d86477a
simonfalke <ziqibrandonli@gmail.com>
2022-12-25 18:00:34 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 
-fn send_message(stream: &mut TcpStream, message: &str) {
+fn send_message(stream: &mut TcpStream, message: &String) {
     stream.write(message.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
@@ -11,19 +11,23 @@ fn send_message(stream: &mut TcpStream, message: &str) {
 fn get_command() -> String {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
+
     input
 }
 
-
 fn receive(stream: &mut TcpStream) -> String {
-    let mut buffer = [0; 512];
     let mut data = String::new();
     loop {
-        let bytes_read = stream.read(&mut buffer).unwrap();
-        if bytes_read == 0 {
+        let mut buffer = [0; 512];
+        stream.read(&mut buffer).unwrap();
+        let converted = String::from_utf8_lossy(&buffer).to_string();
+
+        if converted == "MESSAGEDONE" {
             break;
         }
-        data.push_str(&String::from_utf8_lossy(&buffer[..bytes_read]));
+
+        println!("pushed");
+        data.push_str(&converted);
     }
 
     data
